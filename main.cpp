f00ae6a16c14aa87d7b4275d2cf469327fad42d8
alexkhazzam <akhazzam1@student.gn.k12.ny.us>
2021-01-09 17:53:33 -0500

    init commit

--- main.cpp
+++ main.cpp
@@ -17,12 +17,21 @@ public:
     Player(std::string name_val = "None", int health_val = 0, int xp_val = 0);
     Player(const Player &source);
 
-    ~Player() { std::cout << "Destructor called for Player: " << _name << std::endl; }
+    ~Player()
+    {
+        std::cout << "Destructor called for Player: " << _name << std::endl;
+    }
 };
 
-Player::Player(std::string name_val, int health_val, int xp_val) : _name{name_val}, _health{health_val}, _xp{xp_val} { std::cout << "Three-args constructor called for " << _name << std::endl; }
+Player::Player(std::string name_val, int health_val, int xp_val) : _name{name_val}, _health{health_val}, _xp{xp_val}
+{
+    std::cout << "Three-args constructor called for " << _name << std::endl;
+}
 
-Player::Player(const Player &source) : _name{source._name}, _health{source._health}, _xp{source._xp} { std::cout << "\nCopy constructor - made a copy of " << _name << std::endl; }
+Player::Player(const Player &source) : _name{source._name}, _health{source._health}, _xp{source._xp}
+{
+    std::cout << "\nCopy constructor - made a copy of " << _name << std::endl;
+}
 
 void display_player(Player p)
 {
